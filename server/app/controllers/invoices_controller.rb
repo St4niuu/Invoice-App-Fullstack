020af157589322f64126985e724bfb7295ab2b78
Staniuu <staniuu123@gmail.com>
2023-06-11 10:26:02 +0200

    developing: api's done

--- server/app/controllers/invoices_controller.rb
+++ server/app/controllers/invoices_controller.rb
@@ -9,12 +9,34 @@ class InvoicesController < ApplicationController
 
   def create
     body = JSON.parse(request.body.read)
-    @invoice = Invoice.new
+    @invoice = Invoice.new(user_id: current_user.id)
     body.each do |key, value|
       @invoice[key.to_sym] = value
     end
     if @invoice.save
       render json: {status: 200, message: 'Record added.'}, status: :ok
+    else
+      render json: {status: 500, message: 'Error occured.'}, status: :internal_server_error
+    end
+  end
+
+  def destroy
+    @invoice = Invoice.find_by(id: params[:id], user_id: current_user.id)
+    if @invoice.destroy
+      render json: {status: 200, message: 'Record deleted.'}, status: :ok
+    else
+      render json: {status: 500, message: 'Error occured.'}, status: :internal_server_error
+    end
+  end
+
+  def update
+    body = JSON.parse(request.body.read)
+    @invoice = Invoice.find_by(id: params[:id], user_id: current_user.id)
+    body.each do |key, value|
+      @invoice[key.to_sym] = value
+    end
+    if @invoice.save
+      render json: {status: 200, message: 'Record modified.'}, status: :ok
     else
       render json: {status: 500, message: 'Error occured.'}, status: :internal_server_error
     end
